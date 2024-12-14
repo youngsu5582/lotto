8fffb9d767bede65a823c0acbfa3a19aa37af5ce
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-12-14 22:44:11 +0900

    feat: 로또 구현, 엔티티<->도메인 값 컨버터 구현

---
+++ src/main/kotlin/lotto/domain/LottoNumberConverter.kt
@@ -0,0 +1,16 @@
+package lotto.domain
+
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Converter
+
+@Converter
+class LottoNumbersConverter : AttributeConverter<List<Int>, String> {
+
+    override fun convertToDatabaseColumn(attribute: List<Int>?): String {
+        return attribute?.joinToString(",") ?: ""
+    }
+
+    override fun convertToEntityAttribute(dbData: String?): List<Int> {
+        return dbData?.split(",")?.map { it.trim().toInt() } ?: emptyList()
+    }
+}
---
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -0,0 +1,51 @@
+package lotto
+
+import lotto.domain.Lotto
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+class LottoTest {
+
+    @Test
+    fun `숫자들은 자동으로 정렬된다`() {
+        val lotto = Lotto(numbers = listOf(32, 44, 1, 4, 2, 3))
+        assertThat(lotto).isEqualTo(Lotto(listOf(1, 2, 3, 4, 32, 44)))
+    }
+
+    @Test
+    fun `숫자는 1부터 45까지 가능하다`() {
+        assertThatThrownBy {
+            Lotto(
+                numbers = listOf(1, 2, 3, 4, 5, 46)
+            )
+        }
+    }
+
+    @Test
+    fun `중복된 숫자는 가질수 없다`() {
+        assertThatThrownBy {
+            Lotto(numbers = listOf(1, 2, 3, 4, 44, 44))
+        }
+    }
+
+    @Test
+    fun `숫자는 6개여야 한다`() {
+        assertThatThrownBy {
+            Lotto(numbers = listOf(1, 2, 3, 4, 44, 32, 45))
+        }
+    }
+
+    @Test
+    fun `콤마(,)를 기준으로 문자열로 변환한다`() {
+        val lotto = Lotto(numbers = listOf(1, 2, 3, 4, 32, 44))
+        assertThat(lotto.toStringWithComma()).isEqualTo("1,2,3,4,32,44")
+    }
+
+    @Test
+    fun `숫자들이 같으면 같은걸로 판단한다`() {
+        val lotto1 = Lotto(1, numbers = listOf(1, 2, 3, 4, 32, 44))
+        val lotto2 = Lotto(2, numbers = listOf(1, 2, 3, 4, 32, 44))
+        assertThat(lotto1).isEqualTo(lotto2).isEqualTo(Lotto(listOf(1, 2, 3, 4, 32, 44)))
+    }
+}
