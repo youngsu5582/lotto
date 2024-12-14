161f45a7dfd64504df8d86df5d43f426b0f88087
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-12-14 22:42:25 +0900

    feat: java 8 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,11 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 kotlin {
