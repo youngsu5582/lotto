fc3ed0434a19d2635aee427dcf72e7654d49f2db
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-12-14 22:56:26 +0900

    feat: 로또 회차 정보 구현

---
+++ src/test/kotlin/lotto/domain/LottoRoundInfoTest.kt
@@ -0,0 +1,22 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import java.time.LocalDateTime
+import kotlin.test.Test
+
+class LottoRoundInfoTest {
+    @Test
+    fun `구매 가능시간 보다 지나면 참을 반환한다`() {
+        val ldt = LocalDateTime.now()
+        val edt = ldt.plusDays(1)
+        val lottoRoundInfo = LottoRoundInfo(
+            round = 1,
+            startDate = ldt,
+            endDate = edt,
+            drawDate = ldt.plusDays(2),
+            paymentDeadline = ldt.plusYears(1),
+            status = LottoStatus.ONGOING
+        )
+        assertThat(lottoRoundInfo.isAfter(edt.plusNanos(1))).isTrue()
+    }
+}
