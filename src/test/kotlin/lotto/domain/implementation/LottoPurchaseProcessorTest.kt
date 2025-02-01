9ddef8624b48e1fe4e48378538d6856160e5f3df
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2025-02-02 01:36:30 +0900

    feat: 주문 검증 외부로 분리

--- src/test/kotlin/lotto/domain/implementation/LottoPurchaseProcessorTest.kt
+++ src/test/kotlin/lotto/domain/implementation/LottoPurchaseProcessorTest.kt
@@ -5,90 +5,25 @@ import config.ImplementationTest
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.spring.SpringExtension
-import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
-import lotto.domain.vo.Currency
-import lotto.domain.vo.PurchaseType
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.springframework.context.annotation.Import
 import purchase.domain.PurchaseException
-import purchase.domain.PurchaseExceptionCode
 import purchase.domain.entity.Purchase
 import purchase.domain.entity.PurchaseInfo
-import purchase.domain.implementation.OrderDataRequest
 import purchase.domain.implementation.PurchaseProcessor
-import purchase.domain.implementation.PurchaseValidator
 import purchase.domain.repository.PurchaseRepository
 import purchase.domain.vo.PaymentMethod
 import purchase.domain.vo.PurchaseProvider
-import purchase.domain.vo.PurchaseRequest
 import java.math.BigDecimal
 
 @ImplementationTest
 @Import(TestConfig::class)
 class LottoPurchaseProcessorTest(
     private val purchaseProcessor: PurchaseProcessor,
-    private val purchaseValidator: PurchaseValidator,
     private val purchaseRepository: PurchaseRepository
 ) : FunSpec({
-    val orderId = "orderId"
-    val paymentKey = "paymentKey"
-    val amount = BigDecimal(1000)
     extensions(listOf(SpringExtension))
 
-    context("주문에 대한 사전 정보가 있는지 확인한다") {
-        beforeTest {
-            purchaseValidator.saveTemporary(
-                request = OrderDataRequest(
-                    orderId = orderId,
-                    amount = amount
-                )
-            )
-        }
-        test("사전 정보와 일치한지 검증한다.") {
-            val result = purchaseProcessor.purchase(
-                PurchaseRequest(
-                    purchaseType = PurchaseType.CARD.name,
-                    amount = BigDecimal(1000),
-                    orderId = orderId,
-                    paymentKey = paymentKey,
-                    currency = Currency.KRW.name
-                )
-            )
-            result.isSuccess() shouldBe true
-            result.getId() shouldNotBe null
-        }
-
-        test("orderId 에 해당하는 값이 없으면 예외를 발생한다.") {
-            val exception = shouldThrow<PurchaseException> {
-                purchaseProcessor.purchase(
-                    PurchaseRequest(
-                        purchaseType = PurchaseType.CARD.name,
-                        amount = amount,
-                        orderId = "not Exist",
-                        paymentKey = paymentKey,
-                        currency = Currency.KRW.name
-                    )
-                )
-            }
-            exception.purchaseExceptionCode shouldBe PurchaseExceptionCode.NOT_EXIST_ORDER_ID
-        }
-
-        test("Id 에 해당하는 금액이 다르면 예외를 발생한다.") {
-            val exception = shouldThrow<PurchaseException> {
-                purchaseProcessor.purchase(
-                    PurchaseRequest(
-                        purchaseType = PurchaseType.CARD.name,
-                        amount = BigDecimal(1500),
-                        orderId = orderId,
-                        paymentKey = paymentKey,
-                        currency = Currency.KRW.name
-                    )
-                )
-            }
-            exception.purchaseExceptionCode shouldBe PurchaseExceptionCode.NOT_VALID_ORDER_ID
-        }
-    }
     context("결제 취소") {
         val failPurchase = purchaseRepository.save(
             Purchase(
