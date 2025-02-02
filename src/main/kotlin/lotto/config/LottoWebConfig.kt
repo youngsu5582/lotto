9a9c1f8911e4f36f11faab1712e6002b137dcbd2
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2025-02-02 16:03:21 +0900

    feat: 웹 보안 설정 추가

--- src/main/kotlin/lotto/config/LottoWebConfig.kt
+++ src/main/kotlin/lotto/config/LottoWebConfig.kt
@@ -2,6 +2,7 @@ package lotto.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
@@ -10,16 +11,15 @@ import org.springframework.web.filter.CorsFilter
 class LottoWebConfig {
 
     @Bean
-    fun corsFilter(): CorsFilter {
+    fun corsFilter(environment: Environment): CorsFilter {
         val config = CorsConfiguration()
-        config.allowedOrigins = listOf("http://localhost:3000")
+        config.allowedOrigins = listOf(environment.getProperty("cors.allowed-origins"))
         config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        config.allowedHeaders = listOf("*")
+        config.allowedHeaders = listOf("Authorization", "Content-Type", "Accept")
         config.allowCredentials = true
 
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", config) // 모든 엔드포인트에 적용
-
         return CorsFilter(source)
     }
 }
