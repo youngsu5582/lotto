d9e8441d7876f96c7b621f6dd26e23a6e7734fd2
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2025-02-07 00:07:47 +0900

    test: 테스트 케이스 추가

--- src/test/kotlin/member/controller/MemberInfoTest.kt
+++ src/test/kotlin/member/controller/MemberInfoTest.kt
@@ -13,13 +13,15 @@ import org.junit.jupiter.api.Test
 @AcceptanceTest(["/acceptance/member.json"])
 class MemberInfoTest {
     // 의도적으로 시간을 매우 늘려서 사용자의 토큰을 받아놓았다.(expire : 16000000000000000)
-    private val fixedToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZmMzYWQ4Mi1lM2JhLTQ1MjAtYTk2OS0wYjRhMjkzYzlhOGIiLCJpYXQiOjE3Mzg4NTIyMzIsImV4cCI6MTYwMDE3Mzg4NTIyMzJ9.bzacSBSyi3gakhKau6-m2fZTY-VllRzq0m5Crf0-ZGs"
+    private val token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZmMzYWQ4Mi1lM2JhLTQ1MjAtYTk2OS0wYjRhMjkzYzlhOGIiLCJpYXQiOjE3Mzg4NTIyMzIsImV4cCI6MTYwMDE3Mzg4NTIyMzJ9.bzacSBSyi3gakhKau6-m2fZTY-VllRzq0m5Crf0-ZGs"
+    // 의도적으로 시간을 매우 짧게 사용자의 토큰을 받아놓았다.(expire: 10)
+    private val expiredToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZmMzYWQ4Mi1lM2JhLTQ1MjAtYTk2OS0wYjRhMjkzYzlhOGIiLCJpYXQiOjE3Mzg4NTMxMDksImV4cCI6MTczODg1MzEwOX0.I3ej_tEDUKtDpp7C2DowJGI6EI_F3RIRPhmwUhRYMmU"
     @Test
     fun `토큰을 통해 사용자의 정보를 받는다`() {
         DocsApiBuilder("info-success")
             .setRequest("/api/auth", HttpMethod.GET) {
                 headers{
-                    "Authorization" type DocsFieldType.STRING means "인증 토큰" value "Bearer $fixedToken"
+                    "Authorization" type DocsFieldType.STRING means "인증 토큰" value "Bearer $token"
                 }
             }
             .setResponse {
@@ -30,5 +32,33 @@ class MemberInfoTest {
             }
             .execute()
             .statusCode(200)
+    }
+
+    @Test
+    fun `없는 토큰을 담을시 실패한다`() {
+        DocsApiBuilder("info-fail-not-valid-token")
+            .setRequest("/api/auth", HttpMethod.GET) {
+                headers{
+                    "Authorization" type DocsFieldType.STRING means "인증 토큰" value "Bearer Invalid"
+                }
+            }
+            .setResponse {
+            }
+            .execute()
+            .statusCode(400)
+    }
+
+    @Test
+    fun `만료된 토큰을 담을시 실패한다`() {
+        DocsApiBuilder("info-fail-not-valid-token")
+            .setRequest("/api/auth", HttpMethod.GET) {
+                headers{
+                    "Authorization" type DocsFieldType.STRING means "인증 토큰" value "Bearer $expiredToken"
+                }
+            }
+            .setResponse {
+            }
+            .execute()
+            .statusCode(400)
     }
 }
