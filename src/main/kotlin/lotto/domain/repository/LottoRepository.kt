968f68c49485471e5f424c14263812b4af9e2b72
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2025-02-14 13:00:03 +0900

    feat: 소문자로 변경

--- src/main/kotlin/lotto/domain/repository/LottoRepository.kt
+++ src/main/kotlin/lotto/domain/repository/LottoRepository.kt
@@ -6,12 +6,12 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
 
 interface LottoRepository : CrudRepository<Lotto, Long> {
-    @Query("SELECT * FROM Lotto l WHERE l.numbers IN :numbers", nativeQuery = true)
+    @Query("SELECT * FROM lotto l WHERE l.numbers IN :numbers", nativeQuery = true)
     fun findLottoByNumbersIn(
         @Param("numbers") numbers: List<String>,
     ): List<Lotto>
 
-    @Query("SELECT * FROM Lotto l WHERE l.numbers = :number", nativeQuery = true)
+    @Query("SELECT * FROM lotto l WHERE l.numbers = :number", nativeQuery = true)
     fun findLottoByNumber(
         @Param("number") number: String,
     ): Lotto?
